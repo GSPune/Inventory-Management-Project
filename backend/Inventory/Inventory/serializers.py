ce3edd8838498eee7094215e6bb5a8c92b581601
Stephen George <stephengeorgepune@gmail.com>
2024-02-16 10:37:12 +0530

    Added Project files with APIs

---
+++ backend/Inventory/Inventory/serializers.py
@@ -0,0 +1,18 @@
+#DRF’s Serializers convert model instances to Python
+#dictionaires, which can then be rendered in various API appropriate formats - like JSON
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    '''A ModelSerializer is a subclass of the Serializer class that provides a shortcut for 
+      creating serializers that deal with model instances and querysets. It is a great tool 
+      for generating JSON output directly from the database model, and 
+      for creating new model instances from JSON input.
+    
+      The Meta class within the serializer class specifies the model to be serialized 
+      the fields to be included in the serialized output.'''
+    class Meta(object):
+        model = User
+        fields = ['id','username','is_superuser']
+        #fields = ['id','username','password','email']
+        
---
+++ backend/Inventory/Inventory/views.py
@@ -0,0 +1,51 @@
+from .serializers import UserSerializer
+from django.http import HttpResponse,JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.shortcuts import get_object_or_404
+from django.core.mail import send_mail
+from rest_framework.authentication import SessionAuthentication
+
+@api_view(['POST'])
+def login(request):
+    authentication_classes = (SessionAuthentication,)
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({"Details":"Not Found."},status=status.HTTP_404_NOT_FOUND)
+    #token
+    #login(request, user)
+    serializer = UserSerializer(instance=user)
+    return Response(serializer.data)
+
+'''
+username = request.data['username']
+password = request.data['password']
+
+# Check if username and password are correct, returning User object if so
+user = authenticate(request, username=username, password=password)
+
+if user:
+    login(request, user)
+    serializer = UserSerializer(instance=user)
+    return Response({serializer.data})
+else:
+    return Response({"Details":"Not Found."},status=status.HTTP_404_NOT_FOUND)
+'''
+@api_view(['POST'])
+def sendemail(request):
+    #email = request.data.get('email')
+    email = request.data['email']
+    send_mail(
+    "New Cashier Registration",
+    "",
+    "settings.EMAIL_HOST_USER",
+    [email],
+    fail_silently=False,
+    html_message= "Here is the link to the sign up page.Email address should already be auto-filled!<br>"
+    "<a href=\"http://127.0.0.1:3000/register/\">Click Here!</a>"
+    )
+    return Response({"Success":"True","email":email},status=status.HTTP_200_OK)
+
