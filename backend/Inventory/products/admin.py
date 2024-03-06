657f17c44f570f21af890f7b924899d73e81c264
Stephen George <stephengeorgepune@gmail.com>
2024-03-06 12:34:17 +0530

    Updated admin.py

--- backend/Inventory/products/admin.py
+++ backend/Inventory/products/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Products
 
 class Products_Admin(admin.ModelAdmin):
-    list_display = ("id", "Product_name", "Product_price", "Quantity","Units","Expiry_Date","Date")
+    list_display = ("id", "Product_name", "Product_price", "Quantity","Units","Category","Expiry_Date","Date")
 # Register your models here.
 admin.site.register(Products,Products_Admin)
