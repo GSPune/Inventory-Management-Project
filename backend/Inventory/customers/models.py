aa93d412369efb58bc126d60fdabd173702b5e15
Stephen George <stephengeorgepune@gmail.com>
2024-03-27 22:35:07 +0530

    Sample model -- to be updated

--- backend/Inventory/customers/models.py
+++ backend/Inventory/customers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    Name = models.CharField(max_length=64)
+    Email = models.EmailField(max_length=254, **options)
+    Mobile = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f"{self.id}: {self.Name} ,Email: {self.Email}"
