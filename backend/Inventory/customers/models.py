c9721eaaa4eb5487c61abab93cb9ab3e5a2b2df1
Stephen George <stephengeorgepune@gmail.com>
2024-03-27 22:38:07 +0530

    update

--- backend/Inventory/customers/models.py
+++ backend/Inventory/customers/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     Name = models.CharField(max_length=64)
-    Email = models.EmailField(max_length=254, **options)
+    # Email = models.EmailField(max_length=254, **options)
+    Email = models.EmailField(max_length=254)
     Mobile = models.CharField(max_length=12)
 
     def __str__(self):
