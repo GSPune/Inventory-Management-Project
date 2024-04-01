4ab3e80bdc289bb1518ae5a74f1cb17945dd6c1b
Stephen George <stephengeorgepune@gmail.com>
2024-04-01 21:59:11 +0530

    Created 'Add new customer' API

--- backend/Inventory/customers/views.py
+++ backend/Inventory/customers/views.py
@@ -1,21 +1,21 @@
-# from django.shortcuts import render
-# from rest_framework import status,serializers
-# from .serializers import CustomerSerializer 
+from django.shortcuts import render
+from rest_framework import status,serializers
+from .serializers import CustomerSerializer 
 # from datetime import date
-# #from .models import Products
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
-# # Create your views here.
+from .models import Customer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# Create your views here.
 
-# # Create your views here.
-# @api_view(['POST'])
-# def add_customer(request):
-#     if request.method == 'POST':
-#         #Customer Serializer TO BE CREATED!
-#         serializer = CustomerSerializer(data = request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response({"Added":"Successfully","id":serializer.data['id'],"name":serializer.data['Product_name']}
-#                         ,status=status.HTTP_201_CREATED)#return success message
-#     #else
-#     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+# Create your views here.
+@api_view(['POST'])
+def add_customer(request):
+    if request.method == 'POST':
+        #Customer (De)serializer converts json data to a (python) model instance
+        serializer = CustomerSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"Added":"Successfully","id":serializer.data['id'],"name":serializer.data['Name']}
+                        ,status=status.HTTP_201_CREATED)#return success message
+    #else
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
