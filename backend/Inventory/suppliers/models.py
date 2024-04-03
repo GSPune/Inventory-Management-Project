9b2d62efa695a010e80247d62eb039aa32b6662d
Stephen George <stephengeorgepune@gmail.com>
2024-04-03 22:20:19 +0530

    Model for supplier

--- backend/Inventory/suppliers/models.py
+++ backend/Inventory/suppliers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Supplier(models.Model):
+    Name = models.CharField(max_length=64)
+    # Email = models.EmailField(max_length=254, **options)
+    Email = models.EmailField(max_length=254)
+    Mobile = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f"{self.id}: Name : {self.Name} ,Email : {self.Email}"
