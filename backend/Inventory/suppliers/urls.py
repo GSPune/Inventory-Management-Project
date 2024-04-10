8e722c0783742354ecd403975bac801d620c8c21
Stephen George <stephengeorgepune@gmail.com>
2024-04-10 23:54:56 +0530

    APIs for adding and listing suppliers

--- backend/Inventory/suppliers/urls.py
+++ backend/Inventory/suppliers/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('add/',views.add_supplier),
+    path('add/',views.add_supplier),
+    path('list/',views.list_api)
 ]
--- backend/Inventory/suppliers/views.py
+++ backend/Inventory/suppliers/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework import status,serializers
+from .serializers import SupplierSerializer 
+# from datetime import date
+from .models import Supplier
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['POST'])
+def add_supplier(request):
+    if request.method == 'POST':
+        #Supplier(De)serializer converts json data to a (python) model instance
+        serializer = SupplierSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"Added":"Successfully","id":serializer.data['id'],"name":serializer.data['Name']}
+                        ,status=status.HTTP_201_CREATED)#return success message
+    #else
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def list_api(request):
+    if request.method == 'GET':
+        suppliers = Supplier.objects.all()
+        serializer = SupplierSerializer(suppliers,many=True)
+        #return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data,status=status.HTTP_200_OK)
