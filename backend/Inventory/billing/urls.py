bd2a23036b7d6d8ae6e1e8b6437b33cd4192bc5a
Stephen George <stephengeorgepune@gmail.com>
2024-04-13 20:54:48 +0530

    APIs for listing valid products and customers (for billing purposes)

--- backend/Inventory/billing/urls.py
+++ backend/Inventory/billing/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # path('new-bill/',views.sales_bill),
-    # path('get-products/',views.products),
-    # path('get-customers/',views.customers),
+    path('get-products/',views.products),
+    path('get-customers/',views.customers),
 ]
--- backend/Inventory/billing/views.py
+++ backend/Inventory/billing/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse,JsonResponse
+from rest_framework import status,serializers
+from products.serializers import ProductSerializer 
+from customers.serializers import CustomerSerializer
+from .models import Products,Customer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
+
+@api_view(['GET'])
+def products(request):
+    #Check for out of stock products
+    if request.method == 'GET':
+        prod = Products.objects.filter(Quantity__gt = 0)
+        serializer = ProductSerializer(prod,many=True)
+        #return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def customers(request):
+    if request.method == 'GET':
+        customers = Customer.objects.all() #database-abstraction API that lets you create, retrieve, update and delete objects
+        serializer = CustomerSerializer(customers,many=True)
+        #return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data,status=status.HTTP_200_OK)
