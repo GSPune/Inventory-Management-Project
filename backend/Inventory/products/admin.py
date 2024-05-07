4d707126cd7caac75781818313716cdabd1a2bff
Stephen George <stephengeorgepune@gmail.com>
2024-05-08 00:11:01 +0530

    Testing/learning custom admin actions

--- backend/Inventory/products/admin.py
+++ backend/Inventory/products/admin.py
@@ -3,5 +3,13 @@ from .models import Products
 
 class Products_Admin(admin.ModelAdmin):
     list_display = ("id", "Product_name", "Product_price", "Quantity","Units","Category","Expiry_Date","Date")
+    actions = ["update_products"]
+    #change/update feature already available in Django Admin
+    @admin.action(description="Update products")
+    def update_products(self, request, queryset):
+        pass
+        # print (request)
+        # queryset.update(Product_price=?)
+
 # Register your models here.
 admin.site.register(Products,Products_Admin)
